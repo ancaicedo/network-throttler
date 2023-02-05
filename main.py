acf47a0f70059219f0f477a58e4290d00c6eded3
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2023-02-05 05:12:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import subprocess
 
 
 def get_local_ip_address():
@@ -8,7 +9,7 @@ def get_local_ip_address():
     return s.getsockname()[0]
 
 
-def get_connected_ips():
+def get_connected_ip():
     local_ip = get_local_ip_address()
     subnet = ".".join(local_ip.split(".")[:-1]) + "."
     result = os.popen(f"arp-scan --localnet --interface={get_local_interface()} | awk '/{subnet}/ {{print $1}}'").read()
@@ -20,10 +21,24 @@ def get_local_interface():
     return "ens3"
 
 
+def throttle_download_rate(ip_address, rate_kbps):
+    """Throttle the download rate of a connection with a specified IP address."""
+    subprocess.run(["tc", "qdisc", "add", "dev", "eth0", "root", "handle", "1:", "htb"], check=True)
+    subprocess.run(
+        ["tc", "class", "add", "dev", "eth0", "parent", "1:", "classid", "1:1", "htb", "rate", f"{rate_kbps}kbps"],
+        check=True)
+    subprocess.run(
+        ["tc", "filter", "add", "dev", "eth0", "protocol", "ip", "parent", "1:0", "prio", "1", "u32", f"match", "ip",
+         "dst", ip_address, "flowid", "1:1"], check=True)
+
+
 def main():
     # Example usage
-    connected_ips = get_connected_ips()
-    print(connected_ips)
+    ip_to_throttle = get_connected_ip()
+    print(ip_to_throttle)
+    # rate_kbps = 100
+    # throttle_download_rate(ip_address, rate_kbps)
+    # print(f"Throttled download rate of connection with IP address {ip_address} to {rate_kbps} kilobits per second.")
 
 
 if __name__ == "__main__":
